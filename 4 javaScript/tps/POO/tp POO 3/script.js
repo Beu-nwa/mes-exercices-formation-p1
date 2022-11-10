d4426b23f406002b5505c95b7d4b7cc87214cad1
Benoit Combe <benoit.combe59400@gmail.com>
2022-11-10 11:40:28 +0100

    modif poo 3

--- 4 javaScript/tps/POO/tp POO 3/script.js
+++ 4 javaScript/tps/POO/tp POO 3/script.js
@@ -13,31 +13,65 @@ let contactsList = [
     inputMail,
     tableBodyContent = document.querySelector("#tableBodyContent");
 
+function clearForm() {
+    document.querySelector(`#defaultRadio`).checked = true;
+    document.querySelector(`#inputFirstName`).value = "";
+    document.querySelector(`#inputLastName`).value = "";
+    document.querySelector(`#inputDateOB`).value = "";
+    document.querySelector(`#inputPhone`).value = "";
+    document.querySelector(`#inputMail`).value = "";
+}
+
+function Delete(x) {
+let contact = contactsList.find(y => y.email == x)
+    if (confirm(`Voulez vous supprimer ${contact.firstName} ${contact.lastName} ?`)) {
+        contactsList.splice(contact, 1);
+        tableGeneration();
+    }
+}
+
 function tableGeneration() {
     affichage = "";
+    let i = 0;
     for (const item of contactsList) {
-        affichage += `<tr><td>${item.genre}</td><td>${item.firstName}</td><td>${item.lastName}</td><td>${item.dateob}</td><td>${item.phone}</td><td>${item.email}</td></tr>`;
-    }
+        i++;
+        affichage += `<tr>
+    <td>${item.genre}</td>
+    <td>${item.firstName}</td>
+    <td>${item.lastName}</td>
+    <td>${item.dateob}</td>
+    <td>${item.phone}</td>
+    <td>${item.email}</td>
+    <td><button class="btn btn-outline-danger" onclick="Delete('${item.email}')"><i class="fa-solid fa-trash"></i></button></td>
+    </tr>`;}
     tableBodyContent.innerHTML = affichage;
+}
+
+function isSelected() {
+    return inputGender != "none" && inputFirstName && inputLastName && inputDateOB && inputPhone && inputMail ? true : false;
 }
 
 document.querySelector(`#addBtn`).onclick = () => {
 
-    try {
-        inputGender = document.querySelector(`input[name="gender"]:checked`).value
-    } catch (error) {
-        console.log(error.message);
-        inputGender = "";
-    }
+    // try {
+    //     inputGender = document.querySelector(`input[name="gender"]:checked`).value;
+    // } catch (error) {
+    //     console.log(error.message);
+    //     inputGender = "";
+    // }
     // finally {
     // permet d'executer du code quelque soit s'il y a une erreur ou non
     // }
 
     // inputGender = document.querySelector(`input[name="gender"]:checked`).value? document.querySelector(`input[name="gender"]:checked`).value : "" ;
     // inputGender = document.querySelector(`.radioInput`).value;
+
+    inputGender = document.querySelector(`input[name="gender"]:checked`).value
     inputFirstName = document.querySelector(`#inputFirstName`).value;
     inputLastName = document.querySelector(`#inputLastName`).value;
-    inputDateOB = document.querySelector(`#inputDateOB`).value;
+    inputDateOB = new Date(document.querySelector(`#inputDateOB`).value).toLocaleDateString();
+    // inputDateOB = document.querySelector(`#inputDateOB`).value.split("-");
+    // inputDateOB = inputDateOB[2] + "/" + inputDateOB[1] + "/" + inputDateOB[0];
     inputPhone = document.querySelector(`#inputPhone`).value;
     inputMail = document.querySelector(`#inputMail`).value;
 
@@ -50,14 +84,11 @@ document.querySelector(`#addBtn`).onclick = () => {
                 inputDateOB,
                 inputPhone,
                 inputMail));
+        tableGeneration();
+        clearForm();
     }
     else alert(`Il manque un parametre dans la saisie du nouveau contact`)
-
-    tableGeneration();
 }
 
-function isSelected() {
-    return inputGender && inputFirstName && inputLastName && inputDateOB && inputPhone && inputMail ? true : false;
-}
-
+window.Delete = Delete ;
 window.onload = tableGeneration();
